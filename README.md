6c16fd9644af5a97fab7787f75f939ad0316f5a5
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-04-07 09:46:57 +0500

    js compariosn and logical statements

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aktiWebBatch7
 we did html css and bootstrap basics
+
+js comarison and logical statements
---
+++ week4/day2comparison.js
@@ -0,0 +1,31 @@
+// cpu
+console.log(12 < 10) // false
+
+console.log(12 > 10) // true
+
+console.log(12 >= 12) // true
+
+console.log(10 == 23) // false
+
+console.log(23 == 23) // true 
+
+console.log(34 != 34) // false
+
+
+// ram
+
+let num1 = 10
+let num2 = 23
+
+console.log(num1 < num2)
+console.log(num1 > num2)
+console.log(num1 >= num2)
+console.log(num1 <= num2)
+console.log(num1 == num2)
+
+
+// logical conditions
+
+console.log(12<10 || 12>10) // or, true
+console.log(12<10 && 12>10) // or, false
+console.log(!(12<10)) // not, true
