f7afb383bd0272406ee473b5cddad174db0b9fe3
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-04-21 10:28:00 +0500

    string trim with for loop

--- week5/utility.js
+++ week5/utility.js
@@ -5,3 +5,15 @@ console.log(roomKey)
 
 console.log(anyThingElse)
 
+let firstName = `salman irfan     `
+
+for (let i =0; i<firstName.length; i++) {
+    console.log(firstName[i])
+}
+
+if (firstName[firstName.length-1] === ' ') {
+    console.log(`space founded at the last`)
+    firstName = firstName.trim()
+}
+console.log(firstName)
+
