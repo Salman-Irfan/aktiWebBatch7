028d71d0e0a6ab87c15842605db58cfb13506468
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-05-04 10:31:40 +0500

    axios

---
+++ week7/promises.js
@@ -0,0 +1,11 @@
+// yt
+// fb
+// google map
+// github 
+// chatgpt =? openai api 
+
+// josn place holder
+// forms pree api
+
+const response = axios.get("https://jsonplaceholder.typicode.com/todos/1")
+console.log(response)
