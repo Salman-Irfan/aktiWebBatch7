ebb2b053fee5218159513e37ff16623455a88a1a
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-05-04 10:08:57 +0500

    set iterval messages counts overview

--- week7/setInterval.js
+++ week7/setInterval.js
@@ -22,3 +22,20 @@ const handleChangedTitle = () => {
 setInterval(handleOriginalTitle, 1000);
 
 setInterval(handleChangedTitle, 1700)
+
+let messages = ["abc", "sdf", "def"]
+let oldCount = messages.length // 3
+messages.push("qwe", "ghj")
+let newCount = messages.length // 4
+
+let showNotifNum = newCount - oldCount // 5-3 =2
+if (newCount > oldCount) {
+    showNotifNum = newCount - oldCount // 5-3 =2
+}
+
+const showNotif = document.getElementById("num").innerHTML = showNotifNum
+
+
+notidBtn.addEventListener ("click", () =>{
+    document.getElementById("num").innerHTML = ""
+})
