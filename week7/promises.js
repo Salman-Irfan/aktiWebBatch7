048b9bdbe519e9f957849a16f9d9803bfad86203
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-05-05 09:56:53 +0500

    apis

--- week7/promises.js
+++ week7/promises.js
@@ -7,5 +7,50 @@
 // josn place holder
 // forms pree api
 
-const response = axios.get("https://jsonplaceholder.typicode.com/todos/1")
-console.log(response)
+// const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
+// console.log(response)
+
+let displayApiData = async () => {
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
+        console.log(response)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+
+displayApiData()
+
+
+const contactForm = document.getElementById("contactForm")
+
+const handleFormSubmit = async (event) => {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Get the form elements
+    const emailInput = contactForm.elements["email"];
+    const messageTextarea = contactForm.elements["message"];
+
+    const formObj = {
+        email: emailInput.value,
+        message: messageTextarea.value
+    }
+    console.log(formObj)
+
+    try {
+        const contactApiResponse = await axios.post("https://formspree.io/f/xyyrolea", formObj) 
+        console.log(contactApiResponse)
+        if (contactApiResponse.data.ok){
+            alert(`form submitted successfully`)
+        }else{
+            alert(`faled`)
+        }
+    } catch (error) {
+        console.log(error.message)
+        alert(`internal server error, 500`)
+    }
+}
+
+contactForm.addEventListener("submit", handleFormSubmit)
