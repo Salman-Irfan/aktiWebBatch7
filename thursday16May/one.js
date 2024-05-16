ee9bb60714bddd822e6c7e02eefe9fdb4fa47e42
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-05-16 22:28:28 +0500

    why server

---
+++ thursday16May/one.js
@@ -0,0 +1,8 @@
+const textAboutJewelery = document.getElementById("textAboutJewelery")
+
+textAboutJewelery.innerHTML = `
+    <p>
+        step back into childhood with our clay jewellery collection
+        Each piece rings back memories
+    </p>
+`
